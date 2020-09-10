0c9c77cb1e99a7f6a3708365a22907d81172d973
mtrn <mtrn@mtrn.com>
2020-09-10 08:08:31 -0300

    Condicional pra reservar adicionado

---
+++ reservar.php
@@ -0,0 +1,45 @@
+<?php // se a quantidade de reservas for menor que a quantidade máxima, aparece o formulário de cadastr se não, aparece esgotado
+include('quantidade.php');
+
+$maximo =  $reserva->quantidade;
+$quantidade = $reserva->getQuantidade();
+
+if(isset($_POST['enviar'])):{
+
+  $query = "INSERT INTO reserva(nome, email) VALUES (?,?)";
+  $prep = $conn->prepare($query);
+  $prep->bindValue(1, $_POST['nome']);
+  $prep->bindValue(2, 4_POST['email']);
+  if($prep->execute()):{
+  	echo "Sua reserva foi concluída com sucesso";
+  } endif;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+if($maximo < $quantidade){
+
+echo "<form action ='' method ='post'>";
+echo "<input type = 'nome' id='nome' name='nome' required='true'>";
+echo "<input type = 'email' id='email' name='email' required='true'>";
+echo "<input type = 'submit' id='enviar' name='enviar' required='true'>";
+echo "</form>";
+
+
+
+
+
+
+} else{
+	echo 'Esgotado';
+}
